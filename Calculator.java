d6e019ba3d53abf29f153bb6576cad43d0ac36c2
Anshul Agarwal <anshulagarwal393@gmail.com>
2025-09-22 21:11:31 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static double add(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public static double subtract(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double multiplication(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public static double division(double n1, double n2) {
+        return n1 / n2;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String exit = "Y";
+
+        while (exit.equalsIgnoreCase("Y")) {
+            System.out.println("Enter the first number : ");
+            double n1 = sc.nextDouble();
+
+            System.out.println("+");
+            System.out.println("-");
+            System.out.println("*");
+            System.out.println("/");
+            System.out.println("Choose the operation :");
+            String s1 = sc.next();
+            
+
+            System.out.println("Enter the second number : ");
+            double n2 = sc.nextDouble();
+
+            if (s1.equals("+")) {
+                System.out.println(add(n1, n2));
+            } else if (s1.equals("-")) {
+                System.out.println(subtract(n1, n2));
+            } else if (s1.equals("*")) {
+                System.out.println(multiplication(n1, n2));
+            } else if (s1.equals("/")) {
+                System.out.println(division(n1, n2));
+            }
+            else{
+                System.out.println("You asshole k pkode, choose right operator");
+            }
+
+            System.out.println("Do u want to continue ? (Y/N)");
+            exit = sc.next();
+        }
+        sc.close();
+    }
+}
---
+++ Calculator/AreaCalculator.java
@@ -0,0 +1,69 @@
+package Calculator;
+import java.util.Scanner;
+
+public class AreaCalculator {
+
+    public static double rectangle(double L, double B) {
+        return L * B;
+    }
+
+    public static double square(double L) {
+        return Math.pow(L, 2);
+    }
+
+    public static double triangle(double L, double B) {
+        return B * L / 2;
+    }
+
+    public static double circle(double R) {
+        return Math.PI * Math.pow(R, 2);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ask = "Y";
+
+        while (ask.equalsIgnoreCase("Y")) {
+            String shape = "";
+            boolean validShape = false;
+
+            while (!validShape) {
+                System.out.println("Choose shape :" + " square," + " rectangle," + " triangle," + " circle");
+                shape = sc.next().toLowerCase();
+                if (shape.equals("square") || (shape.equals("rectangle")) || (shape.equals("circle"))
+                        || (shape.equals("triangle"))) {
+                    validShape = true;
+                } else {
+                    System.out.println("Invalid shape bhai, shi se dal phir se.");
+                }
+            }
+            if (shape.equals("circle")) {
+                System.out.println("Enter the radius :");
+                double R = sc.nextDouble();
+                System.out.println(circle(R));
+            } else if (shape.equals("square")) {
+                System.out.println("Enter the length : ");
+                double L = sc.nextDouble();
+                System.out.println(square(L));
+            } else {
+                System.out.println("Enter the length : ");
+                double L = sc.nextDouble();
+
+                System.out.println("Enter the Breadth :");
+                double B = sc.nextDouble();
+
+                if (shape.equals("rectangle")) {
+                    System.out.println(rectangle(L, B));
+                } else if (shape.equals("triangle")) {
+                    System.out.println(triangle(L, B));
+                }
+            }
+    
+            System.out.println("Wanna continue ? (Y/N)");
+            ask = sc.next();
+
+        }
+        sc.close();
+    }
+}
